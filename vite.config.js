80cf4d18dd484f3c40c445ddf891cb21c1d1c28a
Sasha_Gerzhik <girasik3000@gmail.com>
2025-08-19 19:24:43 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'my-app',
 })
-// vite.config.js
-export default {
-  base: 'my-app', 
-}
